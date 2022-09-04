a305328e416d642b44447428f2f79103c18c36ed
dembA7 <93755653+dembA7@users.noreply.github.com>
2022-09-03 22:34:19 -0500

    Update insertion.h

--- insertion.h
+++ insertion.h
@@ -9,6 +9,7 @@
 // All Rights Reserved. May be reproduced for any non-commercial
 // purpose.
 // =================================================================
+
 #ifndef INSERTION_H
 #define INSERTION_H
 
@@ -21,6 +22,7 @@
 // @param A, an array of T elements.
 // @param size, the number of elements in the array.
 // =================================================================
+
 template <class T>
 void insertionSort(T *arr, int size) {
 	for(int i = 1; i < size; i++){
@@ -35,13 +37,17 @@ void insertionSort(T *arr, int size) {
 //
 // @param A, a vector of T elements.
 // =================================================================
+
 template <class T>
-void insertionSort(std::vector<T> &v) {
+int insertionSort(std::vector<T> &v) {
+  int kIs = 0;
 	for(int i = 1; i < v.size(); i++){
 		for(int j = i; j > 0 && v[j] < v[j - 1]; j--){
 			swap(v, j, j - 1);
+      kIs++;
 		}
 	}
+  return kIs;
 }
 
 #endif /* INSERTION_H */
